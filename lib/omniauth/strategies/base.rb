383713ad7fa49728c56ad7aef2177f563d33746f
feiping <feipinghuang@gmail.com>
2015-07-07 13:02:18 +0800

    fix access_token errcode 40001

--- lib/omniauth/strategies/base.rb
+++ lib/omniauth/strategies/base.rb
@@ -36,10 +36,11 @@ module OmniAuth
         @raw_info ||= begin
           access_token.options[:mode] = :query
           if %w(snsapi_userinfo snsapi_login).include? access_token["scope"]
+            check_access_token!
             response = access_token.get("/sns/userinfo", :params => {"openid" => @uid}, parse: :text)
             @raw_info = JSON.parse(response.body.gsub(/[\u0000-\u001f]+/, ''))
           else
-            @raw_info = {"openid" => @uid }
+            @raw_info = { "openid" => @uid }
           end
         end
       end
@@ -53,6 +54,15 @@ module OmniAuth
           'grant_type' => 'authorization_code'
           }.merge(token_params.to_hash(symbolize_keys: true))
         client.get_token(params, deep_symbolize(options.auth_token_params))
+      end
+
+      def check_access_token!
+        response = access_token.get("/sns/auth", :params => {"openid" => @uid}, parse: :json).parsed
+        unless response["errcode"] == 0 && response["errmsg"] == 'ok'
+          access_token.client.options[:token_url] = "/sns/oauth2/refresh_token"
+          self.access_token = access_token.refresh!(appid: client.id, parse: :json)
+          access_token.client.options[:token_url] = "/sns/oauth2/access_token"
+        end
       end
     end
   end
