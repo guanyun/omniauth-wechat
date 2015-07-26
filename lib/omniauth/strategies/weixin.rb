8591b5709659b094fd88c78515326b9de76863ae
feiping <feipinghuang@gmail.com>
2015-07-27 00:57:02 +0800

    raise a exception when response errorcode

--- lib/omniauth/strategies/weixin.rb
+++ lib/omniauth/strategies/weixin.rb
@@ -1,8 +1,7 @@
-require 'omniauth/strategies/base'
-
+require "omniauth-oauth2"
 module OmniAuth
   module Strategies
-    class Weixin < OmniAuth::Strategies::Base
+    class Weixin < OmniAuth::Strategies::OAuth2
       option :name, "weixin"
 
       option :client_options, {
@@ -13,6 +12,55 @@ module OmniAuth
       }
 
       option :authorize_params, {scope: "snsapi_userinfo"}
+
+      option :token_params, {parse: :json}
+
+      uid do
+        raw_info['openid']
+      end
+
+      info do
+        {
+          nickname:   raw_info['nickname'],
+          sex:        raw_info['sex'],
+          province:   raw_info['province'],
+          city:       raw_info['city'],
+          country:    raw_info['country'],
+          headimgurl: raw_info['headimgurl']
+        }
+      end
+
+      extra do
+        {raw_info: raw_info}
+      end
+
+      def request_phase
+        params = client.auth_code.authorize_params.merge(redirect_uri: callback_url).merge(authorize_params)
+        params["appid"] = params.delete("client_id")
+        redirect client.authorize_url(params)
+      end
+
+      def raw_info
+        @uid ||= access_token["openid"]
+        @raw_info ||= begin
+          access_token.options[:mode] = :query
+          response = access_token.get("/sns/userinfo", :params => {"openid" => @uid}, parse: :text)
+          @raw_info = JSON.parse(response.body.gsub(/[\u0000-\u001f]+/, ''))
+          fail(response.body) if @raw_info[:errcode]
+          @raw_info
+        end
+      end
+
+      protected
+      def build_access_token
+        params = {
+          'appid' => client.id,
+          'secret' => client.secret,
+          'code' => request.params['code'],
+          'grant_type' => 'authorization_code'
+          }.merge(token_params.to_hash(symbolize_keys: true))
+        client.get_token(params, deep_symbolize(options.auth_token_params))
+      end
     end
   end
 end
